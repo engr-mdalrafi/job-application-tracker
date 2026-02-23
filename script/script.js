d9d4c0bcc5714e26d17a983d349305636912db7e
engr-mdalrafi <engr.mdalrafi@gmail.com>
2026-02-23 10:11:02 +0600

    some issue

--- script/script.js
+++ script/script.js
@@ -5,6 +5,8 @@ let totalCount = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+let numberOfJobe = document.getElementById('numbe-of-job');
+
 const allFilterBtn = document.getElementById('all-filter-btn');
 const interviewFilterBtn = document.getElementById('interview-filter-btn');
 const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
@@ -20,8 +22,10 @@ const mainContainer = document.querySelector('main');
 
 function calculateCount (){
     totalCount.innerText = allCardSection.children.length;
+    numberOfJobe.innerText = allCardSection.children.length;
     interviewCount.innerText = interviewList.length;
     rejectedCount.innerText = rejectedList.length;
+
 
 }
 calculateCount();
@@ -39,7 +43,40 @@ function toggleStyle(id) {
 
     selected.classList.remove('bg-[#FFFFFF]', 'text-[#64748B]');
     selected.classList.add('bg-[#3B82F6]', 'text-white');
-
 }
+
+mainContainer.addEventListener('click', function(event){
+    // console.log();
+    const parentNode = event.target.parentNode.parentNode;
+    const jobTitle = parentNode.querySelector('.job-title').innerText;
+    const jobDesignation = parentNode.querySelector('.job-designation').innerText;
+    const selary = parentNode.querySelector('.selary').innerText;
+    const status = parentNode.querySelector('.status').innerText;
+    const jobDescription = parentNode.querySelector('.job-description').innerText;
+    
+    const cardInfo = {
+        jobTitle, 
+        jobDesignation, 
+        selary, 
+        status, 
+        jobDescription,
+    };
+
+    // console.log(cardInfo);
+
+    const jobExist = interviewList.find(item => item.jobTitlen == cardInfo.jobTitle);
+
+    if(!jobExist){
+        interviewList.push(cardInfo);
+    }
+    // console.log(interviewList);
+
+
+
+
+
+    
+
+})
 
 
