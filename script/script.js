eecf7334f34b5a93438d65d8b0f5fd90f28bb046
engr-mdalrafi <engr.mdalrafi@gmail.com>
2026-02-23 09:09:03 +0600

    toggleing-added

--- script/script.js
+++ script/script.js
@@ -1,17 +1,45 @@
+let interviewList = [];
+let rejectedList = [];
+
 let totalCount = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
 // console.log(totalCount, interviewCount, rejectedCount);
 
 const allCardSection = document.getElementById('all-cards');
-console.log();
+
+// main tare dhorsi
+const mainContainer = document.querySelector('main');
+// console.log(mainContainer);
 
 
 function calculateCount (){
     totalCount.innerText = allCardSection.children.length;
-}
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
 
+}
 calculateCount();
+
+function toggleStyle(id) {
+    allFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    interviewFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    
+    allFilterBtn.classList.add('bg-[#FFFFFF]', 'text-[#64748B]');
+    interviewFilterBtn.classList.add('bg-[#FFFFFF]', 'text-[#64748B]');
+    rejectedFilterBtn.classList.add('bg-[#FFFFFF]', 'text-[#64748B]');
+
+    const selected = document.getElementById(id);
+
+    selected.classList.remove('bg-[#FFFFFF]', 'text-[#64748B]');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
+
+}
 
 
